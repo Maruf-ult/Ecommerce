e4d8e605841434ba2cd54ac3adfdf12f9af404bc
maruf_hossain <marufhossain123098@gmail.com>
2024-09-04 16:19:59 +0600

    login with firebase complete

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,13 +1,33 @@
+import { faG } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
-import { faG } from '@fortawesome/free-solid-svg-icons';
+import { auth } from "./firebase";
+import toast, { Toaster } from "react-hot-toast";
 
 function Login() {
+   
+  const [email,setEmail] =useState("")
+  const [pass,setPass] = useState("")
+
   const navigate = useNavigate();
 
   const handleNav = () => {
-    navigate('/');
+    navigate("/");
   };
+
+
+  const handleSubmit =async (e)=>{
+    e.preventDefault();
+    try {
+       await signInWithEmailAndPassword(auth,email,pass)
+       toast.success('logged in successfully')    
+    } catch (error) {
+      toast.error(error.message, { duration: 2000 }); 
+    }
+  }
+
 
   return (
     <>
@@ -23,14 +43,32 @@ function Login() {
         <div className="flex items-center justify-center ml-40 mb-16">
           <div className="flex flex-col bg-slate-200 h-96 w-96 justify-center items-center text-center space-y-5 pb-6 rounded-lg">
             <h1 className="font-bold text-3xl">Sign in</h1>
-            <input type="email" placeholder="email" className="p-3 w-64 rounded-sm" />
-            <input type="password" placeholder="password" className="p-3 w-64 rounded-sm" />
+            <input
+              type="email"
+              placeholder="email"
+              className="p-3 w-64 rounded-sm"
+              value={email}
+              onChange={(e)=>setEmail(e.target.value)}
+
+            />
+            <input
+              type="password"
+              placeholder="password"
+              className="p-3 w-64 rounded-sm"
+              value={pass}
+              onChange={(e)=>setPass(e.target.value)}
+            />
 
             <div className="flex flex-col space-y-2">
-              <button className="bg-sky-500 py-2 w-64 rounded-md text-white ml-1">Login</button>
+              <button onClick={handleSubmit} className="bg-sky-500 py-2 w-64 rounded-md text-white ml-1">
+                Login
+              </button>
               <p className="pl-20 font-light">
-                New here?{' '}
-                <button onClick={handleNav} className="text-sky-500 font-bold underline">
+                New here?{" "}
+                <button
+                  onClick={handleNav}
+                  className="text-sky-500 font-bold underline"
+                >
                   Create account
                 </button>
               </p>
@@ -38,12 +76,17 @@ function Login() {
 
             <p className="font-light">-or continue with-</p>
             <button className="bg-sky-500 text-white py-2 w-52 rounded-md">
-              <FontAwesomeIcon icon={faG} className="bg-blue-600 p-1 mr-2" style={{ verticalAlign: 'middle' }} />
+              <FontAwesomeIcon
+                icon={faG}
+                className="bg-blue-600 p-1 mr-2"
+                style={{ verticalAlign: "middle" }}
+              />
               Sign in with Google
             </button>
           </div>
         </div>
       </div>
+      <Toaster></Toaster>
     </>
   );
 }
